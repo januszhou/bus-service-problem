0a550f7ac812714275905686e96d06bde52297c6
janus <janus.zhou@wisebanyan.com>
2014-10-01 23:55:00 -0400

    inital commit for test cpp file

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+	return 0;
+}
+
+void person()
+{
+
+}
+
+void person()
+{
+
+}
+
+void person()
+{
+
+}
